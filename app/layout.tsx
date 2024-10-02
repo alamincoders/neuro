e451a8391a506cd0056be17b6fb4eab03549cd24
Al Amin <alaminpgmr@gmail.com>
2024-10-02 18:03:53 +0600

    build(deps): update layout.tsx, package-lock.json and package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ConvexClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="neuro-theme-next-ts">
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
