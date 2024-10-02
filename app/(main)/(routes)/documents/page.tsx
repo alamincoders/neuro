0fec64149339b0f7a7c6aa9bf25bb1235a569fa6
Al Amin <alaminpgmr@gmail.com>
2024-10-02 17:16:23 +0600

    up: added empty document page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentPage = () => {
-  return <div>DocumentPage</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image src="/empty.svg" alt="empty" height={300} width={300} className="dark:hidden" />
+      <Image src="/empty-dark.svg" alt="empty" height={300} width={300} className="hidden dark:block" />
+
+      <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s workspace - get started by creating a note.</h2>
+      <Button className="inline-flex items-center">
+        <PlusCircle className="mr-2 h-4 w-4" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentPage;
