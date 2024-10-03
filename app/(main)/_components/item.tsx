d9254722b97c249ee7627e3a5e4715013a451a27
Al Amin <alaminpgmr@gmail.com>
2024-10-03 07:38:55 +0600

    update item.tsx

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -5,11 +5,31 @@ interface ItemProps {
   label: string;
   icon: LucideIcon;
 }
+/**
+ * A menu item with an icon, label, and onClick handler.
+ *
+ * @example
+ * <Item onClick={() => {}} label="Example" icon={ChevronRight} />
+ *
+ * @param {() => void} onClick - The callback to call on click.
+ * @param {string} label - The text to display next to the icon.
+ * @param {LucideIcon | null | undefined} icon - The icon to display next to the label.
+ */
 const Item = ({ onClick, label, icon: Icon }: ItemProps) => {
+  if (!Icon) {
+    throw new Error(
+      "Item component requires 'icon' prop to be a valid LucideIcon."
+    );
+  }
+
   return (
     <div
       style={{ paddingLeft: "12px" }}
-      onClick={onClick}
+      onClick={(event) => {
+        // Stop the event from bubbling up to the parent if the user clicks on the icon
+        event.stopPropagation();
+        onClick();
+      }}
       role="button"
       className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium cursor-pointer"
     >
