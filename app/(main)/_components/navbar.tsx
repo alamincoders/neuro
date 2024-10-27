cd7f0fcde1ff2648ccc72521aa17d42d4097e3c5
Al Amin <alaminpgmr@gmail.com>
2024-10-27 18:58:19 +0600

    "Added Banner component to Navbar and wrapped nav element in a fragment"

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
+import Banner from "./banner";
 import Title from "./title";
 
 interface NavbarProps {
@@ -31,18 +32,22 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   }
 
   return (
-    <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
-      {isCollapsed && (
-        <MenuIcon
-          role="button"
-          onClick={onResetWidth}
-          className="w-6 h-6 text-muted-foreground"
-        />
-      )}
-      <div className="flex items-center justify-between w-full">
-        <Title initialData={document} />
-      </div>
-    </nav>
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="w-6 h-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+
+      {document.isArchived && <Banner documentId={document._id} />}
+    </>
   );
 };
 
