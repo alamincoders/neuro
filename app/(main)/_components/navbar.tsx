a027041427874fb70bfaab3b807847d804002bdc
Al Amin <alaminpgmr@gmail.com>
2024-10-27 19:17:11 +0600

    "Updated navbar and button components: renamed MenuIcon to Menu, added Menu component to navbar, and refactored button styles and imports."

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -3,9 +3,10 @@
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
-import { MenuIcon } from "lucide-react";
+import { Menu as MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import Banner from "./banner";
+import Menu from "./menu";
 import Title from "./title";
 
 interface NavbarProps {
@@ -21,8 +22,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4 justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -43,6 +47,9 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
 
